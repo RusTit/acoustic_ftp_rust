00115a9698cd13007172f9e7dd22698983e5bc07
Russkih Anatoly <titanofdarkness@gmail.com>
2020-10-27 16:04:35 +1000

    Prototyping.

--- src/acoustic/models.rs
+++ src/acoustic/models.rs
@@ -17,4 +17,31 @@ impl AccessToken {
         let duration = Duration::from_secs(self.expires_in.into());
         self.expire_at = Some(now.add(duration));
     }
+
+    pub fn is_outdated(&self) -> bool {
+        if let Some(expire_at) = self.expire_at {
+            let now = SystemTime::now();
+            return now > expire_at
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod acoustic_tests {
+    use super::*;
+
+    #[test]
+    fn check_is_outdated_default() {
+        let token: AccessToken = Default::default();
+        assert_eq!(token.is_outdated(), true);
+    }
+
+    #[test]
+    fn check_is_outdated() {
+        let mut token: AccessToken = Default::default();
+        token.expires_in = 10;
+        token.recalc_expire_at();
+        assert_eq!(token.is_outdated(), false);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     let provider = AcousticProvider::new(&settings);
     let token = provider.get_access_key().await;
     match token {
-        Ok(token) => info!("Token: {:?}", token),
+        Ok(token) => info!("Token: {} is valid: {}", token.access_token, token.is_outdated()),
         Err(e) => error!("Error getting token: {:?}", e),
     }
 
